ddf05cd74e21979576647caf9caa47d367465a88
Sword <verigo.dmitry@gmail.com>
2025-03-06 23:38:51 +0300

    Added lesson4.py

---
+++ lesson4.py
@@ -0,0 +1,17 @@
+price = int(input("Введите цену: "))
+is_vip = str(input("Вы VIP? (да/нет) - ")).lower()
+discount = None
+
+if price >= 1000:
+    if is_vip == "да":
+        discount = 0.25
+    else:
+        discount = 0.2
+elif price >= 500 and price < 1000:
+    discount = 0.1
+elif price < 500:
+    discount = 0
+else:
+    print("Ошибка!")
+
+print(f"Товар на {price} со скидкой {discount * 100}% будет стоить {price * (1 - discount)}")
